dc8def611ef18c90d8c6f6104359a397bbe7e3d1
Christopher Abreu <cfa911@outlook.pt>
2018-05-25 23:12:31 +0100

    fds

--- tp6/tp6/MyTerrain.js
+++ tp6/tp6/MyTerrain.js
@@ -6,29 +6,22 @@
 
 class MyTerrain extends CGFobject{
 	
-	constructor(scene)
+	constructor(scene,nrDivs,vector = [])
 	{
 		super(scene);
-		this.plano = new Plane(this.scene, 100, 0, 6, 0, 6);
-		/*
-		this.chao = new CGFappearance(this);
-        this.chao.setAmbient((1/5)*(220/255),(1/5)*(20/255),(1/5)*(60/255));
-		this.chao.setDiffuse((3/5)*(220/255),(3/5)*(20/255),(3/5)*(60/255),1);
-		this.chao.setSpecular((8/10)*(220/255),(8/10)*(20/255),(8/10)*(60/255),1);
-		this.chao.setShininess(10);
-		this.chao.loadTexture("resources/images/terrain.png");*/
-	};
+		this.nrDivs = nrDivs;
+		this.vector = vector;
+		this.plano = new Plane(this.scene, nrDivs, 0, 1, 0, 1);
 	
+		
+	};
+
 	display()
 	{
-		
-		
-		 this.scene.pushMatrix();
-    // this.rotate(-90 * degToRad, 1, 0, 0);
-    // this.scale(32, 32, 0.2);
-    //his.chao.apply();
-     this.plano.display();
-     this.scene.popMatrix();
+		this.plano.indices = this.vector.slice(); //copIar vetor, não está a funcionar não sei porquê
+		this.scene.pushMatrix();
+     	this.plano.display();
+    	this.scene.popMatrix();
 
 	};
 }
